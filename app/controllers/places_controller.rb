a9328b5671201a64f6b9efb92b459dd3efd5a2e2
Michael Kulinski <me@michaelkulinski.com>
2015-12-29 23:00:15 +0000

    Error messages added to form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,9 +10,13 @@ class PlacesController < ApplicationController
   	end
 
   	def create
-    	current_user.places.create(place_params)
-    	redirect_to root_path
-  	end
+  		@place = current_user.places.create(place_params)
+  		if @place.valid?
+    		redirect_to root_path
+  		else
+    		render :new, :status => :unprocessable_entity
+  		end
+	end
 
   	def show
   		@place = Place.find(params[:id])
@@ -32,7 +36,11 @@ class PlacesController < ApplicationController
   			return render :text => 'Not Allows', :status => :forbidden
   		end
   		@place.update_attributes(place_params)
-  		redirect_to root_path
+  		if @place.valid?
+  			redirect_to root_path
+  		else
+  			render :edit, :status => :unprocessable_entity
+  		end
   	end
 
   	def destroy
