ee74d5a7cb27c412f5c201f23190d93f8dd40dc1
Michael Kulinski <me@michaelkulinski.com>
2016-01-06 03:14:23 +0000

    Issues with comment test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "comment creation functionality" do
+  	
+  	user = FactoryGirl.create(:user)
+  	sign_in user
+
+  	place = FactoryGirl.create(:place)
+
+    assert_difference 'Comment.count' do
+      post :create, {:comment => {
+          :message => 'Great Place!',
+		  :rating => '1_star',
+        }
+      }
+    end
+	assert_redirected_to places_path
+    assert_equal 1, user.place.comments.count
+  	
+  end
+
 end
--- test/factories.rb
+++ test/factories.rb
@@ -4,4 +4,11 @@ FactoryGirl.define do
 		password "logitech05"
 		password_confirmation "logitech05"
 	end
+
+	factory :place do 
+		name "Coffee Coffee Coffee"
+		address "421 W Broadway, Long Beach, CA, 90802"
+		description "The coffeeest coffee"
+	end
+
 end
