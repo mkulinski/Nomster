482da5e4d9a9ec1294b61c10f3d70af4bb168a6a
Michael Kulinski <me@michaelkulinski.com>
2016-01-06 01:06:39 +0000

    added a test for comments and a test for unique user page

---
+++ test/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do 
+	factory :user do 
+		email "me@michaelkulinski.com"
+		password "logitech05"
+		password_confirmation "logitech05"
+	end
+end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "humanized rating" do
+  	rating = Comment.new(:rating => '1_star').humanized_rating
+    expected = 'one star'
+    actual = rating
+    assert_equal expected, actual
+  end
 end
